56f0aa07871a0363a9f110612886d623ecd27a80
SovereignSteve <51929007+SovereignSteve@users.noreply.github.com>
2022-05-19 23:27:33 -0400

    Service and Config Updates

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class defaults:
    apikey = "H4PV22WR9MSNYJM25TN9FFXEZGB3NFDX6V"
    address = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
-   offset = 100
+   offset = 1000
    page = 1
 
 class database:
-   host = "192.168.1.105"
-   port = 49154
+   host = "192.168.1.105"   
+   port = 49157
    user = "pyservice"
    pw = "dev"
    name = "postgres"
--- service.py
+++ service.py
@@ -18,6 +18,9 @@ class ServiceEngine(object):
       self.page = page
       self.offset = offset      
 
+      printDetail("Database Connection String(database=" + config.database.name + ", user=" + config.database.user + ", host=" + 
+      config.database.host + ", port=" + str(config.database.port) + ")")
+
       signal.signal(signal.SIGINT, self.startShutdown)
       signal.signal(signal.SIGTERM, self.startShutdown)
  
@@ -60,7 +63,11 @@ class ServiceEngine(object):
       
       printInfo("Archiving (" + str(len(txData)) + ") internal transactions!")
 	  
+<<<<<<< HEAD
+      try:             
+=======
       try:    
+>>>>>>> 3a33d2dcab02ab9ae601c6eb22b9e53c3b2ac5db
          pgdb = psycopg2.connect(database=config.database.name, 
                                      user=config.database.user, password=config.database.pw, host=config.database.host, port=config.database.port)
          pgdb.autocommit = True
@@ -71,7 +78,11 @@ class ServiceEngine(object):
          print(error)
       finally:
          if pgdb is not None:
+<<<<<<< HEAD
+            pgdb.close()
+=======
                pgdb.close()
+>>>>>>> 3a33d2dcab02ab9ae601c6eb22b9e53c3b2ac5db
 
    def run(self):  
       while not engine.shutdown:
